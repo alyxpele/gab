30b6a4c849dbd4837e9a604b8c6915f60a30a22e
Alyx PELE <alyx.pele@outlook.fr>
2025-09-18 00:09:00 +0200

    feat(components/utils): improve cva

--- src/components/utils/cva.config.ts
+++ src/components/utils/cva.config.ts
@@ -1,17 +1,36 @@
 import { defineConfig } from 'cva'
 import { twMerge } from 'tailwind-merge'
 
+const debugClasses = twMerge(
+    // 'outline outline-pink-500',
+    // 'bg-pink-700/8',
+    // 'focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-pink-500',
+)
+
 export const { cva, cx, compose } = defineConfig({
     hooks: {
         onComplete: import.meta.env.DEV ? (className) => {
-            const res = twMerge(className)
+            const res = twMerge(className, debugClasses)
 
             if (res !== className) {
-                console.debug('[twMerge]', className, '->', res)
+                const A = className.split(' ')
+                const B = res.split(' ')
+                console.debug('[twMerge]', A.filter((x) => !B.includes(x)), '->', B.filter((x) => !A.includes(x)))
             }
             return res
         } : (className) => twMerge(className),
     },
 })
+
+export const rcx = <T extends string>(
+    classes: Record<T, string>,
+    extend?: string | Partial<Record<T, string>>,
+): Record<T, string> => {
+    return (Object.entries(classes) as Array<[T, string]>)
+        .reduce((r, [key, className]) => {
+            r[key] = cx(className, typeof extend === 'string' ? extend : extend?.[key])
+            return r
+        }, {} as Record<T, string>)
+}
 
 export { type VariantProps } from 'cva'
